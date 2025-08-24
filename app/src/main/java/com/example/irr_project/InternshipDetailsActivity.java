7abcd8ce937bbd7d272492bad33b9274fc835b18
LeQuangTrung <trunglqhe172533@fpt.edu.vn>
2025-08-24 08:53:55 +0700

    Revert "Merge branch 'testing' into Trunglq"
    This reverts commit ff2d20c898876cec1591292b9b70956141db3e54, reversing
    changes made to 250ecc389bba63352976ce2b51bfad4daef9ba13.

--- app/src/main/java/com/example/irr_project/InternshipDetailsActivity.java
+++ app/src/main/java/com/example/irr_project/InternshipDetailsActivity.java
@@ -213,7 +213,6 @@ public class InternshipDetailsActivity extends AppCompatActivity {
 
     // Method to save file to internal storage
     private void saveFileToInternalStorage(Uri fileUri) {
-        buttonApply.setEnabled(true);
         try {
             InputStream inputStream = getContentResolver().openInputStream(fileUri);
             if (inputStream != null) {
@@ -251,9 +250,10 @@ public class InternshipDetailsActivity extends AppCompatActivity {
         String status = dbHelper.getApplicationStatus(userId, internshipId);
         if (status != null) {
             textViewStatus.setText("Trạng thái: " + status);
-            if (!"Rejected".equals(status)) {
-                buttonApply.setEnabled(false);
-            }
+            buttonApply.setEnabled(false);
+        } else {
+            textViewStatus.setText("Trạng thái: Chưa ứng tuyển");
+            buttonApply.setEnabled(true);
         }
     }
 
